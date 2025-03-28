b4b77a036b213da9793598690f72414372ecb618
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-03-28 19:17:55 -0300

    Update ContaCorrente.java

--- src/ContaCorrente.java
+++ src/ContaCorrente.java
@@ -1,14 +1,30 @@
 
 public class ContaCorrente extends Conta {
 
-	public ContaCorrente(Cliente cliente) {
-		super(cliente);
-	}
+    private double limiteCredito;
 
-	@Override
-	public void imprimirExtrato() {
-		System.out.println("=== Extrato Conta Corrente ===");
-		super.imprimirInfosComuns();
-	}
-	
+    // Modificação do construtor para incluir limite de crédito
+    public ContaCorrente(Cliente cliente, double limiteCredito) {
+        super(cliente);
+        this.limiteCredito = limiteCredito;
+    }
+
+    @Override
+    public void sacar(double valor) {
+        // Verifica se o saque não ultrapassa o saldo + o limite de crédito
+        if (valor <= (this.getSaldo() + limiteCredito)) {
+            super.sacar(valor);  // Realiza o saque, mesmo que ultrapasse o saldo
+            System.out.println("Saque realizado: R$ " + valor);
+        } else {
+            System.out.println("Saque inválido: Excede limite de crédito.");
+        }
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Corrente ===");
+        super.imprimirInfosComuns();
+        System.out.println(String.format("Limite de Crédito: %.2f", this.limiteCredito));
+    }
 }
+
