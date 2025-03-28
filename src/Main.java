1d1550b2f758cca67828c79c5ada1c8aae48d8f5
Leonardo Felipe Ribas Ferreira <Leonardo_felipe1@outlook.com>
2025-03-28 19:18:42 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+    public static void main(String[] args) {
+        Cliente venilton = new Cliente();
+        venilton.setNome("Venilton");
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
-	}
+        // Criação da Conta Corrente com limite de crédito de R$ 500,00
+        Conta cc = new ContaCorrente(venilton, 500);
+        
+        // Criação da Conta Poupança
+        Conta poupanca = new ContaPoupanca(venilton);
 
-}
+        // Operações de conta corrente
+        cc.depositar(100);
+        cc.transferir(100, poupanca);
+        
+        // Operações de saque e transferência
+        cc.sacar(1200); // Saque permitido dentro do limite de crédito (Saldo + Limite de Crédito)
+        cc.sacar(1800); // Saque inválido (Excede limite de crédito)
+
+        // Exibindo extratos
+        cc.imprimirExtrato();
+
